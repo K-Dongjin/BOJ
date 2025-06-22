1ad72fe7df54f3dc7151258d2175dfbb84d676ff
K-Dongjin <84118165+K-Dongjin@users.noreply.github.com>
2025-06-22 23:43:50 +0900

    '특별한 도시' 별도 관리 / 시간복잡도는 동일하나, 실행 시간은 줄어듦 -> 272ms

--- G4_16958.py
+++ G4_16958.py
@@ -5,7 +5,12 @@ def manhattans(a, b):
   return abs(a[1] - b[1]) + abs(a[2] - b[2])
 
 n, t = map(int, input().split())
-pList = [(-1, -1, -1)] + [ tuple(map(int, input().split())) for _ in range(n) ]
+pList = [(-1, -1, -1)]
+scList = []
+for _ in range(n):
+  s, x, y = map(int, input().split())
+  pList.append( (s, x, y) )
+  if (s == 1): scList.append( (1, x, y) )
 
 for _ in range(int(input())):
   a, b = map(int, input().split())
@@ -15,26 +20,19 @@ for _ in range(int(input())):
     ans = min(ans, t)
 
   elif (pList[a][0] == 1):
-    for i in range(1, n+1):
-      if (i == a): continue
-
-      if (pList[i][0] == 1):
-        temp = t + manhattans(pList[i], pList[b])
-        ans = min(ans, temp)
+    for sc in scList:
+      temp = t + manhattans(sc, pList[b])
+      ans = min(ans, temp)
 
   elif (pList[b][0] == 1):
-    for i in range(1, n+1):
-      if (i == b): continue
-
-      if (pList[i][0] == 1):
-        temp = manhattans(pList[a], pList[i]) + t
+    for sc in scList:
+        temp = manhattans(pList[a], sc) + t
         ans = min(ans, temp)
 
   else:
     idxA, idxB = 0, 0
     disA, disB = 2001, 2001
     for i in range(1, n+1):
-
       if (pList[i][0] == 1):
         temp = manhattans(pList[a], pList[i])
         if (temp < disA):
